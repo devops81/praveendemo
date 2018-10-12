d4a097aff9063c92aa9bdc80c0657e9c6f0bfb53
devops81 <devops81@gmail.com>
2018-10-12 17:50:17 +0000

    Update demofile.sh

--- demofile.sh
+++ demofile.sh
@@ -1,1 +1,12 @@
-echo "its coming unde rhte shell script"
+#!/bin/bash
+
+current_usage=$( df -h | grep '/dev/simfs' | awk {'print $5'} )
+max_usage=90%
+
+if [ ${current_usage%?} -ge ${max_usage%?} ]; then
+    mailbody="Max usage exceeded. Your disk usage is at ${current_usage}."
+    echo "Sending mail..."
+    echo ${mailbody} | mail -s "Disk alert!" "yourown@mail.tld"
+elif [ ${current_usage%?} -lt ${max_usage%?} ]; then
+    echo "No problems. Disk usage at ${current_usage}." > /dev/null
+fi
